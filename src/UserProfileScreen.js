5faeec4b49dda1b64b0173e83e047ea9731af4d2
Flame <carlcurist@vip.qq.com>
2020-03-24 13:08:07 +0800

    fix logout function

--- src/UserProfileScreen.js
+++ src/UserProfileScreen.js
@@ -10,7 +10,7 @@ export default class UserProfileScreen extends React.Component {
     }
 
     logout() {
-        this.props.navigation.navigate("Scan")
+        this.props.navigation.navigate("Login")
     }
 
     render() {
